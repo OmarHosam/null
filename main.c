733b0f093235bd2e5c6e0bbba45db76cdcc8a396
Omar Hosam <git.hub.alive597@slmail.me>
2024-10-09 22:38:22 +0300

    Reading hardcoded file

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+typedef enum {
+    EXIT,
+    INT_LITERAL,
+    SEMI_COLON,
+    OPEN_BRACKET,
+    CLOSE_BRACKET
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char* value;
+} Token;
+
 int main(void) {
-    printf("Hello, World!\n");
+    FILE* p_file;
+    char current;
+    Token* tokens;
+
+    p_file = fopen("test.nl", "r");
+
+    current = fgetc(p_file);
+    while (current != EOF) {
+        printf("%c", current);
+        current = fgetc(p_file);
+    }
+    fclose(p_file);
 
     return 0;
 }
