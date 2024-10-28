db3401a1be0f0fc22f9c976eb0d35de6a1a62986
Omar Hosam <git.hub.alive597@slmail.me>
2024-10-28 23:27:43 +0300

    fix(parser): Removing overwritten declarations.
    Since we've moved most of the structure's declarations to the header
    files, they must be removed from the source file.

--- src/parser.c
+++ src/parser.c
@@ -1,38 +1,3 @@
 #include "parser.h"
 
-struct NodeExprIntLit {
-    Token int_lit;
-};
-
-// TODO: Refactor this code.
-typedef struct NodeExpr NodeExpr;
-typedef struct NodeStmt NodeStmt;
-
-struct NodeExpr {
-    enum {
-        NODE_EXPR_INT_LIT
-    } tag;
-    union {
-        struct NODE_EXPR_INT_LIT {
-            Token int_lit;
-        } AST_INT_LIT;
-    } data;
-};
-
-struct NodeStmt {
-    enum {
-        NODE_EXIT
-    } tag;
-    union {
-        struct NODE_EXIT {
-            NodeExpr* expr;
-        } AST_INT_LIT;
-    } data;
-};
-
-struct NodeProg {
-    NodeStmt* stmts;
-    int size;
-};
-
 Token* parse(Token* tokens, int* length) { return tokens; }
